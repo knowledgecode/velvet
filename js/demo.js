27a2b3fe782e4b9342841024815d9c3bf6d5d9b9
knowledgecode <knowledgecode@gmail.com>
2016-05-15 23:24:53 +0900

    initial commit

---
+++ js/demo.js
@@ -0,0 +1,96 @@
+/*global velvet, SilkTouch */
+(function () {
+    'use strict';
+
+    var forEach = Array.prototype.forEach;
+
+//    if (velvet.msie) {
+    if (~navigator.userAgent.indexOf('compatible; MSIE 9.')) {
+        forEach.call(document.getElementsByClassName('not-supported'), function (span) {
+            span.style.display = 'inline';
+        });
+    }
+
+    var examples = {};
+    var options = function (element) {
+        var showElement = function () { element.parentElement.parentElement.getElementsByClassName('play')[0].style.display = ''; };
+        return { duration: 2000, easing: 'ease', oncancel: showElement, onfinish: showElement };
+    };
+    var styles = {
+        translateX: function (element) { return velvet(element).weave({ translateX: 200 }, options(element)).cancel(); },
+        translateY: function (element) { return velvet(element).weave({ translateY: 200 }, options(element)).cancel(); },
+        translateZ: function (element) { return velvet(element).weave({ translateZ: 99 }, options(element)).cancel(); },
+        scale: function (element) { return velvet(element).weave({ scale: 10 }, options(element)).cancel(); },
+        scaleX: function (element) { return velvet(element).weave({ scaleX: 10 }, options(element)).cancel(); },
+        scaleY: function (element) { return velvet(element).weave({ scaleY: 10 }, options(element)).cancel(); },
+        scaleZ: function (element) { return velvet(element).style({ scaleZ: 1, rotateY: 60 }).weave({ scaleZ: 11.6 }, options(element)).cancel(); },
+        rotate: function (element) { return velvet(element).weave({ rotate: 180 }, options(element)).cancel(); },
+        rotateX: function (element) { return velvet(element).weave({ rotateX: 180 }, options(element)).cancel(); },
+        rotateY: function (element) { return velvet(element).weave({ rotateY: 180 }, options(element)).cancel(); },
+        rotateZ: function (element) { return velvet(element).weave({ rotateZ: 180 }, options(element)).cancel(); },
+        skewX: function (element) { return velvet(element).weave({ skewX: 75 }, options(element)).cancel(); },
+        skewY: function (element) { return velvet(element).weave({ skewY: 75 }, options(element)).cancel(); },
+        opacity: function (element) { return velvet(element).weave({ opacity: 0 }, options(element)).cancel(); },
+        scroll: function (element) {
+            return velvet().weave([0], [element.scrollHeight - element.clientHeight], options(element), function (d) {
+                element.scrollTop = d[0];
+            }).cancel();
+        },
+        color: function (element) {
+            return velvet().weave([255, 0, 0], [0, 255, 255], options(element), function (d) {
+                d[0] |= 0;
+                d[1] |= 0;
+                d[2] |= 0;
+                element.style.backgroundColor = 'rgb(' + d[0] + ',' + d[1] + ',' + d[2] + ')';
+            }).cancel();
+        }
+    };
+
+    window.addEventListener('load', function () {
+        Object.keys(styles).forEach(function (id) {
+            examples[id] = styles[id](document.getElementById(id));
+        });
+
+        forEach.call(document.querySelectorAll('.play'), function (element) {
+            new SilkTouch(element).on(function () {
+                examples[this.getAttribute('data-example-id')].play();
+                this.parentElement.getElementsByClassName('play')[0].style.display = 'none';
+            }, false);
+        });
+
+        forEach.call(document.querySelectorAll('.pause'), function (element) {
+            new SilkTouch(element).on(function () {
+                examples[this.getAttribute('data-example-id')].pause();
+                this.parentElement.getElementsByClassName('play')[0].style.display = '';
+            }, false);
+        });
+
+        forEach.call(document.querySelectorAll('.cancel'), function (element) {
+            new SilkTouch(element).on(function () {
+                examples[this.getAttribute('data-example-id')].cancel();
+            }, false);
+        });
+
+        forEach.call(document.querySelectorAll('.finish'), function (element) {
+            new SilkTouch(element).on(function () {
+                examples[this.getAttribute('data-example-id')].finish();
+            }, false);
+        });
+
+        forEach.call(document.querySelectorAll('.reverse'), function (element) {
+            new SilkTouch(element).on(function () {
+                var w = examples[this.getAttribute('data-example-id')];
+
+                if (w.direction() > 0) {
+                    this.style.backgroundColor = '#7f7';
+                } else {
+                    this.style.backgroundColor = '';
+                }
+                w.reverse();
+                this.parentElement.getElementsByClassName('play')[0].style.display = 'none';
+            }, false);
+        });
+    });
+
+}());
+
---
+++ js/index.js
@@ -0,0 +1,126 @@
+/*global velvet, SilkTouch */
+(function () {
+    'use strict';
+
+    var onload;
+
+    if (~navigator.userAgent.indexOf('compatible; MSIE 9.')) {
+//    if (velvet.msie) {
+        onload = function () {
+            var cube = document.getElementById('cube');
+            var w = velvet(cube).weave({
+                rotate: 360
+            }, {
+                duration: 1000, easing: 'ease-in-out', onfinish: function () { this.reverse(); }
+            });
+
+            var pause = false;
+
+            new SilkTouch(cube).on(function () {
+                if (pause) {
+                    w.play();
+                } else {
+                    w.pause();
+                }
+                pause = !pause;
+            });
+        };
+//    } else if (~navigator.userAgent.indexOf('compatible; MSIE')) {
+    } else if (~navigator.userAgent.indexOf('compatible; MSIE 1')) {
+        onload = function () {
+            var cube = document.getElementById('cube');
+            var cube1 = velvet(document.getElementById('cube1'));
+            var cube2 = velvet(document.getElementById('cube2'));
+            var cube3 = velvet(document.getElementById('cube3'));
+            var cube4 = velvet(document.getElementById('cube4'));
+            var cube5 = velvet(document.getElementById('cube5'));
+            var cube6 = velvet(document.getElementById('cube6'));
+            var options = {
+                duration: 2500,
+                easing: 'linear',
+                onfinish: function () { this.play(); }
+            };
+
+            var w1 = cube1.style({
+                rotateX: 0, rotateY: 90, rotateZ: 0, transformOrigin: '50% 50% -60px', backfaceVisibility: 'hidden'
+            }).weave({
+                rotateX: 360, rotateY: 450, rotateZ: 0
+            }, options);
+
+            var w2 = cube2.style({
+                rotateX: 0, rotateY: 0, rotateZ: 0, transformOrigin: '50% 50% -60px', backfaceVisibility: 'hidden'
+            }).weave({
+                rotateX: 360, rotateY: 360, rotateZ: 0
+            }, options);
+
+            var w3 = cube3.style({
+                rotateX: 90, rotateY: 0, rotateZ: 0, transformOrigin: '50% 50% -60px', backfaceVisibility: 'hidden'
+            }).weave({
+                rotateX: 450, rotateY: 0, rotateZ: -360
+            }, options);
+
+            var w4 = cube4.style({
+                rotateX: -90, rotateY: 0, rotateZ: 0, translateZ: 0, transformOrigin: '50% 50% -60px', backfaceVisibility: 'hidden'
+            }).weave({
+                rotateX: 270, rotateY: 0, rotateZ: 360
+            }, options);
+
+            var w5 = cube5.style({
+                rotateX: 0, rotateY: 180, rotateZ: 0, transformOrigin: '50% 50% -60px', backfaceVisibility: 'hidden'
+            }).weave({
+                rotateX: 360, rotateY: 540, rotateZ: 0
+            }, options);
+
+            var w6 = cube6.style({
+                rotateX: 0, rotateY: 270, rotateZ: 0, transformOrigin: '50% 50% -60px', backfaceVisibility: 'hidden'
+            }).weave({
+                rotateX: 360, rotateY: 630, rotateZ: 0
+            }, options);
+
+            var pause = false;
+
+            new SilkTouch(cube).on(function () {
+                if (pause) {
+                    w1.play();
+                    w2.play();
+                    w3.play();
+                    w4.play();
+                    w5.play();
+                    w6.play();
+                } else {
+                    w1.pause();
+                    w2.pause();
+                    w3.pause();
+                    w4.pause();
+                    w5.pause();
+                    w6.pause();
+                }
+                pause = !pause;
+            });
+        };
+    } else {
+        onload = function () {
+            var cube = document.getElementById('cube');
+            var w = velvet(cube).weave({
+                rotateX: 1400, rotateY: -1000, rotateZ: 600
+            }, {
+                duration: 10000, easing: 'ease-in-out', onfinish: function () { this.reverse(); }
+            });
+
+            var pause = false;
+
+            new SilkTouch(cube).on(function () {
+                if (pause) {
+                    w.play();
+                } else {
+                    w.pause();
+                }
+                pause = !pause;
+            });
+        };
+    }
+
+    window.addEventListener('DOMContentLoaded', onload);
+
+}());
+
