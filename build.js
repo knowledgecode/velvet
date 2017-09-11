0cbba6baba32144b2a3b2f2d8389c65834971f6a
knowledgecode <knowledgecode@gmail.com>
2017-09-11 23:07:02 +0900

    update the build system

--- build.js
+++ build.js
@@ -1,6 +1,8 @@
-var fs = require('fs');
-var re = /\/\/ \[\[[\s\S\n]+\/\/ \]\]/g;
+let fs = require('fs');
+let re1 = /\/\/ \<\<[\s\S\n]+\/\/ \>\>/;
+let re2 = /\/\/ \[\[[\s\S\n]+\/\/ \]\]/g;
+let src = fs.readFileSync(process.argv[2], 'utf8');
+let bezier = fs.readFileSync('node_modules/bezier-easing/src/index.js', 'utf8');
 
-var text = fs.readFileSync(process.argv[2], 'utf8');
-process.stdout.write(text.replace(re, ''));
+process.stdout.write(src.replace(re1, bezier).replace(re2, ''));
 
